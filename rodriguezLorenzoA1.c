2641fe047c42bbe6925384559d41aef9693fab8c
Lorenzo Rodriguez <lrodri07@uoguelph.ca>
2025-01-25 17:52:52 -0500

    Function 1 is now populating the struct array correctly

--- rodriguezLorenzoA1.c
+++ rodriguezLorenzoA1.c
@@ -3,11 +3,11 @@
 
 /*  TASK 1: Read each line of a1Data.txt into animal struct array starting from position 0. 
 Each struct element will have animalName, features, and classLabel  */
-//MAY NEED TO CHANGE TO -> NOTATION???
+
 int readFromFile (char fName [30], struct Animal dataZoo [NUM_SAMPLES]){
 
-    char name [MAX_LENGTH_ANIMAL_NAME];
-    int line_number;
+    int line_number = 0;
+
 
     // Open file in read mode
     FILE * fPtr;  
@@ -21,22 +21,22 @@ int readFromFile (char fName [30], struct Animal dataZoo [NUM_SAMPLES]){
     
     // Loop through file to gather info until eof is reached
     while (!feof (fPtr)){
-
+        
         // Get animal name and store at element line_number of struct array
         fscanf(fPtr, "%s", dataZoo[line_number].animalName);   
         //strcpy(dataZoo[line_number].animalName, name);   // Add name to struct of a specified line number MAY NEED TO USE THIS IF U GET ERROR
-
+        
         // Get animal features
         for (int i = 0; i<NUM_FEATURES; i++){
-            fscanf(fPtr, "%d", dataZoo[line_number].features[i]);   // Loops 16 times to get and populate features
+            fscanf(fPtr, "%d", &dataZoo[line_number].features[i]);   // Loops 16 times to get and populate features
         }
 
         // Get and store class label
-        fscanf(fPtr, "%d", dataZoo[line_number].classLabel);
-
+        fscanf(fPtr, "%d", &dataZoo[line_number].classLabel);
+        
         line_number++;
     }
-    
+    //printf("Populated animals: %s", dataZoo[line_number].animalName);
     fclose(fPtr);
     return 1;
 }
--- rodriguezLorenzoA1Main.c
+++ rodriguezLorenzoA1Main.c
@@ -7,10 +7,14 @@ int main(){
     int result;
     struct Animal dataZoo [NUM_SAMPLES];
 
-    result = readFromFile("a1Data.txt", &dataZoo[NUM_SAMPLES]);
+    result = readFromFile("a1Data.txt", dataZoo);
 
+    //PRINTS FOR TESTING
     printf("%d", result);
-    printf("%s", dataZoo[0]);
+    printf("%s\n", dataZoo[0].animalName);
+    printf("%s\n", dataZoo[3].animalName);
+    printf("%s\n", dataZoo[20].animalName);
+    
 
     return 0;
 }
