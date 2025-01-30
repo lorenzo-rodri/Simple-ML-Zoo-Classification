338e97c96ed6f095063fb2bc21f4e5f52008020c
Lorenzo Rodriguez <lrodri07@uoguelph.ca>
2025-01-29 23:13:22 -0500

    Function 1 and 2 are working correctly, function 3 is in progress

--- rodriguezLorenzoA1.c
+++ rodriguezLorenzoA1.c
@@ -40,3 +40,60 @@ int readFromFile (char fName [30], struct Animal dataZoo [NUM_SAMPLES]){
     fclose(fPtr);
     return 1;
 }
+
+/* TASK 2: Calculate euclidean distance, hamming distance, and jaccard similarity*/
+void distanceFunctions (int vector1 [NUM_FEATURES], int vector2 [NUM_FEATURES], float * euclideanDistance, int * hammingDistance, float * jaccardSimilarity){
+    int tempResult = 0;
+    float oneMatch, zeroMatch = 0.0;
+
+    // Calculate euclidean distance
+    for (int i=0; i<NUM_FEATURES; i++){
+        tempResult = (vector1[i] - vector2[i]);
+        tempResult = tempResult * tempResult;
+        *euclideanDistance += tempResult;
+    }
+    *euclideanDistance = sqrt(*euclideanDistance);
+    
+    // Calculate hamming distance
+    for (int i=0; i<NUM_FEATURES; i++){
+        if (vector1[i] != vector2[i]){
+            (*hammingDistance)++;
+        }
+    }
+
+    // Calculate Jaccard Similarity
+    for (int i=0; i<NUM_FEATURES; i++){
+        if (vector1[i] == 1 && vector2[i] == 1){
+            oneMatch++;
+        }
+        else if (vector1[i] == 0 && vector2[i] == 0){
+            zeroMatch++;
+        }
+    }
+
+    if (NUM_FEATURES - zeroMatch > 0){
+    *jaccardSimilarity = oneMatch / (NUM_FEATURES - zeroMatch);
+    } 
+    else{
+    *jaccardSimilarity = 0.0; // for the case where all features are 0
+    }
+
+}
+
+/* TASK 3: finds the "k" number of closest neighbors in decreasinf or increaseing order depending on the type
+of distance function. The indices of the associated samples are saved in knearestneighbors*/
+void findKNearestNeighbors (struct Animal dataZoo [NUM_SAMPLES], int newSample [NUM_FEATURES], int k, int whichDistanceFunction, int kNearestNeighbors [NUM_SAMPLES]){
+    float sortedArray[NUM_SAMPLES][NUM_SAMPLES];
+    for (int i=1; i<NUM_SAMPLES; i++){
+        int hammingDistance = 0;
+        float euclideanDistance, jaccardSimilarity = 0.0;
+        distanceFunctions(newSample, dataZoo[i].features, &euclideanDistance, &hammingDistance, &jaccardSimilarity);
+        // If distance function is euclidean or hamming, sort in decreasing order, else, sort in increasing order 
+        if (whichDistanceFunction == 1 || whichDistanceFunction == 2){
+            
+        }
+
+    }
+}
+
+//QUESTIONS: why do i start from i=1 in the findknearestneighbors
--- rodriguezLorenzoA1Main.c
+++ rodriguezLorenzoA1Main.c
@@ -4,17 +4,23 @@
 
 int main(){
 
-    int result;
+    int result, hammingDistance = 0;
+    float euclideanDistance, jaccardSimilarity = 0.0;
     struct Animal dataZoo [NUM_SAMPLES];
 
+    //TESTING Task 1
     result = readFromFile("a1Data.txt", dataZoo);
-
-    //PRINTS FOR TESTING
-    printf("%d", result);
+    printf("%d\n", result);
     printf("%s\n", dataZoo[0].animalName);
     printf("%s\n", dataZoo[3].animalName);
     printf("%s\n", dataZoo[20].animalName);
-    
+
+    //TESTING Task 2
+    distanceFunctions(dataZoo[0].features, dataZoo[1].features, &euclideanDistance, &hammingDistance, &jaccardSimilarity);
+    printf("euc: %.2f\nham: %d\njac: %.2f", euclideanDistance, hammingDistance, jaccardSimilarity);
 
     return 0;
 }
+
+//aardvark 1 0 0 1 0 0 1 1 1 1 0 0 4 0 0 1 1
+//antelope 1 0 0 1 0 0 0 1 1 1 0 0 4 1 0 1 1
